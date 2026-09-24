977622bebacc0532868a9b9d41818676cacf38ce
Txoka <asiertxoka.2@gmail.com>
2026-09-24 20:52:44 +0200

    EMA2

--- symphony/cli.py
+++ symphony/cli.py
@@ -165,7 +165,7 @@ def main(argv=None, *, default_target="symphony", prog="scc"):
                 meter_hz = (
                     instantaneous_hz
                     if meter_hz is None
-                    else meter_hz * 0.95 + instantaneous_hz * 0.05
+                    else meter_hz * 0.99 + instantaneous_hz * 0.01
                 )
                 meter_last_steps = machine.steps
                 meter_last_time = now
